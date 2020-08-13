943731c7c8cf9ec6ce78708ca70061b9a3a43753
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-13 11:25:29 +0300

    add priortyQueue that implement IPriorityQueue

---
+++ LINQPriorityQueue/PriorityQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PriorityQueue.Abstract;
+
+namespace LINQPriorityQueue
+{
+
+    public class PriorityQueue<T> : IPriorityQueue<T>
+    {
+        private List<Tuple<uint, T>> _allPriorities;
+
+        public PriorityQueue()
+        {
+            _allPriorities = new List<Tuple<uint, T>>();
+        }
+
+        public int Count => _allPriorities.Count;
+
+        public T Dequeue()
+        {
+            Tuple<uint, T> priority = _allPriorities.OrderBy(p => p.Item1).ToList().First();
+
+            _allPriorities.Remove(priority);
+
+            return priority.Item2;
+        }
+
+        public void Enqueue(uint priority, T item)
+        {
+            _allPriorities.Add(new Tuple<uint, T>(priority, item));
+        }
+
+        public void RunForeach()
+        {
+            _allPriorities.ForEach(p => Console.WriteLine($"the item: {p.Item2} have the priority {p.Item1}"));
+        }
+    }
+}
---
+++ LINQPriorityQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQPriorityQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<string> priorityQueue = new PriorityQueue<string>();
+            priorityQueue.Enqueue(3, "A");
+            priorityQueue.Enqueue(1, "B");
+            priorityQueue.Enqueue(2, "C");
+            priorityQueue.Enqueue(1, "D");
+
+
+            Console.WriteLine(priorityQueue.Dequeue());
+            priorityQueue.RunForeach();
+        }
+    }
+}
